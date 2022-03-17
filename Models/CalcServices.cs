03de18bb310f6ec4618e13a231960feb451f7e8a
SroMan-io <85176272+SroMan-io@users.noreply.github.com>
2022-03-17 13:14:12 +0700

    Add files via upload

---
+++ Models/CalcServices.cs
@@ -0,0 +1,35 @@
+﻿namespace WebApplication1.Models
+{
+    public class CalcServices
+    {
+        public int x { get; set; }
+        public int y { get; set; }         
+        public char sign { get; set; }
+
+        public string res = "";
+
+        public string Calculate(int x, int y, char s)
+        {
+            int result=0; string res="";
+            if (s == '+') result = (x + y);
+            else if (s == '-') result = x - y;
+            else if (s == '*') result = x * y;           
+            else if (s == '/' && y != 0) result = x / y;
+            res = result.ToString();
+            if (s == '/' && y == 0) res = "На ноль делить нельзя!";
+            return res;
+        }
+
+        public string Calculate(CalcServices calc)
+        {
+            int result = 0; string res = "";
+            if (calc.sign == '+') result = (x + y);
+            else if (calc.sign == '-') result = x - y;
+            else if (calc.sign == '*') result = x * y;
+            else if (calc.sign == '/' && y != 0) result = x / y;
+            res = result.ToString();
+            if (calc.sign == '/' && y == 0) res = "На ноль делить нельзя!";
+            return res;
+        }
+    }
+}
